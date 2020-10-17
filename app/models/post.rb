154e179021e62f591ea3094c20726473236ef3ab
Jade Lynn Masker <jadelynn@masker.family>
2020-10-17 16:34:24 -0400

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,13 @@
 class Post < ActiveRecord::Base
+  validates :title, presence: true
+  validates :content, length: { minimum: 250 }
+  validates :summary, length: { maximum: 250 }
+  validates :category, inclusion: { in: %w[Fiction Non-Fiction] }
+  validate :title_needs_to_be_clickbaity
+
+  def title_needs_to_be_clickbaity
+    unless title&.match(/(Won't Believe)|(Secret)|(Top)|(Guess)/)
+      errors.add(:title, 'needs to be clickbaity')
+    end
+  end
 end
