e5c80bd68b4ded1dfc19790a3a337e591ff1f715
miloscomplex <hicallmesutton@gmail.com>
2020-10-22 21:58:03 -0500

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,19 @@
 class Post < ActiveRecord::Base
+  include ActiveModel::Validations
+
+  validates :title, presence: true
+  validates :content, length: { minimum: 250 }
+  validates :summary, length: { maximum: 250 }
+  validates :category, inclusion: { in: %w(Fiction, Non-Fiction) }
+  validate :clickbait
+
+  def clickbait
+    if title.present?
+      string_arr = ["Won't Believe", "Secret", "Top [number]", "Guess"]
+      if string_arr.any? { |string| title.exclude?(string) }
+        errors.add(:title, "you need the clickbait!")
+      end
+      binding.pry
+    end
+  end
 end
