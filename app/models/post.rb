35338e7360a67e0a321773c36792413fd0436097
sarisgar28 <sarisgar28@gmail.com>
2020-10-26 14:50:05 -0400

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,6 +5,11 @@ validates :title, presence: true
 validates :content, length: {minimum: 250}
 validates :summary, length: {maximum: 250}
 validates :category, inclusion: { in: %w(Fiction Non-Fiction)}
+validate :clickbaity_method
+
+def clickbaity_method
+    if Post.title.valid?
+end
 
 
 
