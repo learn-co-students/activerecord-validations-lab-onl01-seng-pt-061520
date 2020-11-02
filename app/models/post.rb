33f7203e514d2076232ffe90ce0c6352528cfbc1
CelesteSMThomas <celestesmthomas@gmail.com>
2020-11-01 18:16:25 -0800

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,14 +1,21 @@
+require 'pry'
 class Post < ActiveRecord::Base
+    
     validates :title, presence: true
+    validates :title, format: { with: /Won't|Believe|Secret|Top|Guess/}
     validates(:content, {:length => {:minimum => 250}}) 
     validates(:summary, { :length => {:maximum => 250}})
     validates(:category, {:inclusion => {:in => %w(Fiction Non-Fiction)}})
-    validate :is_clickbait?
+    # validate :is_clickbait?
 
-    clickbait_words = ["Won't Believe", "Secret", "Top", "Guess"]
-    def is_clickbait?
-        if :title.exclude?(clickbait_words)
-            errors.add(:title, "must use clickbait words.")
-        end
-    end
+    # @@clickbait_words = ["Won't Believe", "Secret", "Top", "Guess"]
+    # def is_clickbait?
+    #     #binding.pry
+    #     @@clickbait_words.each do |word|
+    #         if title.include?(word)
+    #             return
+    #         end
+    #     end
+    #     errors.add(:title, "must use clickbait words.")
+    # end
 end
