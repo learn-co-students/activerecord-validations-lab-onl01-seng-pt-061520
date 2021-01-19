b9bcff9e179cf47e51f930bf172d991b1185a74b
Celina Jackson <65145705+CelinaJackson@users.noreply.github.com>
2021-01-19 15:54:37 -0500

    Update post.rb

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,8 +1,8 @@
 class Post < ActiveRecord::Base
     validates :title, presence: true
     validates :content, length: {minimum: 250}
-    validates :summary, length: {maximum: 10}
-    validates :category, inclusion: { in: %w(Fiction Non-Fiction), 
+    validates :summary, length: {maximum: 250}
+    validates :category, inclusion: { in: %w(fiction non-fiction), 
        message: "%{value} not a valid category. Category must either be Fiction or Non-Fiction."}
     validates :title_must_be_clickbait, presence: true
 
